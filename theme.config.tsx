48b4075793ff46fbd3892aa67aaf9acd89d12c79
Faqih Muntashir <itsfaqih@gmail.com>
2023-03-21 22:26:34 +0700

    feat: add small introduction and some snippets

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Faqih Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/itsfaqih/personal-docs",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/itsfaqih/personal-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Faqih's Docs",
   },
-}
+};
 
-export default config
+export default config;
